b9981d4c360f20d7874b4a1cc7b4ab5386f99dc7
Arunava Das <arunava_das@persistent.com>
2024-01-16 17:18:28 +0530

    5 topics done, conditionals, day 2

--- 4.conditionals/conditional.rb
+++ 4.conditionals/conditional.rb
@@ -1,32 +1,20 @@
-# Ruby: Conditionals
+#!/usr/bin/ruby
 
-There are various types of if statement in Ruby.
-
-- if statement
-- if-else statement
-- if-else-if (elsif) statement
-- ternay (shortened if statement) statement
-
-## if statement
-```ruby
-a = 12
+puts "\n-------- if statement ----------"
+a = 19
 if a >= 18   
     puts "You are eligible to vote."   
 end
-```
 
-## if else statement
-```ruby
+puts "\n-------- if else statement ----------"
 a = 17
 if a >= 18   
     puts "You are eligible to vote."   
 else   
     puts "You are not eligible to vote."   
 end
-```
 
-## elsif statement
-```ruby
+puts "\n-------- elsif statement ----------"
 a = 77
 if a < 50
     puts "Student is fail"   
@@ -39,60 +27,31 @@ elsif a >= 80 && a <= 90
 elsif a >= 90 && a <= 100   
     puts "Student gets A+ grade"    
 end  
-```
 
-## ternary operator
-```ruby
+puts "\n-------- ternary operator ----------"
 x = (a > 3 ? true : false);    
 puts x
-```
 
-## if modifier
-```
-code if condition
-```
-Executes code if the conditional is true.
-
-```ruby
+puts "\n-------- if modifier ----------"
 $debug = 1
 print "its working\n" if $debug
-```
-**Output**
 
-    its working
-
-## unless statement
-Executes code if conditional is false. If the conditional is true, code specified in the else clause is executed.
-```ruby
+puts "\n-------- unless statement ----------"
 x = 1 
 unless x >= 2
     puts "x is less than 2"
 else
     puts "x is greater than 2"
 end
-```
-**Output**
 
-    x is less than 2
-
-## unless modifier
-Executes code if conditional is false.
-
-```ruby
+puts "\n-------- unless modifier ----------"
 x = 1
 puts "1 -- Value is set\n" if x
 puts "2 -- Value is set\n" unless x
 x = false
 puts "3 -- Value is set\n" unless x
-```
-**Output**
 
-    1 -- Value is set
-    3 -- Value is set
-
-
-## case, when statement
-```ruby
+puts "\n-------- case when ----------"
 $age =  5
 case $age
 when 0 .. 2
@@ -106,8 +65,3 @@ when 13 .. 18
 else
     puts "adult"
 end
-```
-**Output**
-
-    little child
-
