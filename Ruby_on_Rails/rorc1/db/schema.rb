da72628d1dd5aed25978d8c96c25af329612f656
Arunava Das <arunavanew52486@gmail.com>
2024-02-12 12:50:49 +0530

    redone the user model

--- Ruby_on_Rails/rorc1/db/schema.rb
+++ Ruby_on_Rails/rorc1/db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_07_092133) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_12_071221) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
