20442ca82ff94ddc61883bbcb85f164cb7e473bf
Arunava Das <arunavanew52486@gmail.com>
2024-02-13 00:18:14 +0530

    added user signup with customised msg

--- Ruby_on_Rails/rorc1/config/routes.rb
+++ Ruby_on_Rails/rorc1/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :users
   resources :articles
-  # resources :articles
 
   root 'pages#home'
   get 'about', to: 'pages#about'
+  # get 'signup', to: 'users#new'
   
   #! 'route' controller#action - this format
   # root 'application#hello'
