1fbeb0db3cee5ae5889079d0ff1bee79c77386c7
Arunava Das <arunavanew52486@gmail.com>
2024-02-23 12:00:39 +0530

    1-m association added

--- Ruby_on_Rails/rorc3/app/controllers/application_controller.rb
+++ Ruby_on_Rails/rorc3/app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
     $appName = "BlogApp"
     $loremipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    
+    def require_same_user
+        if current_user != @post.user
+            flash[:notice] = "You can only edit or delete your own article"
+            redirect_to @post
+        end
+    end
 end
--- Ruby_on_Rails/rorc3/app/controllers/posts_controller.rb
+++ Ruby_on_Rails/rorc3/app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   # GET /posts or /posts.json
   def index
@@ -23,7 +24,7 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-
+    @post.user = current_user
     respond_to do |format|
       if @post.save
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
